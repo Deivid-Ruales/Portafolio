370e87cc8497a7e16de86d0b42571ffec7b51c7e
Deivid Ruales <davidral33@hotmail.com>
2023-02-16 21:31:43 -0500

    Iniciando proyecto en react

---
+++ src/components/header.js
@@ -0,0 +1,38 @@
+
+
+export const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg" >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Deivid_Ruales</a>
+          <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Curriculum Vitae</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Skills</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Study</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Experience</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">ContactMe</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,19 @@
+import "../styles/welcome.css";
+
+export const Welcome = () => {
+  return (
+    <section className="section section-welcome">
+      <div className="text-welcome">
+        <p className='title-welcome'>
+          Hola!
+        </p>
+        <p className='name-welcome'>
+          Soy Deivid Ruales
+        </p>
+        <p className='profesion-welcome'>
+          Soy Programador Web
+        </p>
+      </div>
+    </section>
+  );
+}
